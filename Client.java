6b9b3a522b3266f95cea63b57c879cbb19b7eef6
DanBest21 <44063921+DanBest21@users.noreply.github.com>
2018-11-02 00:13:22 +0000

    Update Client class to handle multiple clients

--- Client.java
+++ Client.java
@@ -2,31 +2,58 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+// Class that handles the client-side processing of the application.
 public class Client 
 {
-	public static void main(String[] args) 
+	// Instance variables
+	private String hostname;
+	private Integer portNumber;	
+	private Socket socket = null;
+	
+	// Setter method that prompts the user to enter a user name.
+	public String setUsername(Scanner in)
 	{
-		String hostname = "localhost";
-		Integer portNumber = 2066;
-		
-		Socket socket = null;
-		
+		System.out.println("Please enter a username:");
+		return "SET USERNAME " + in.nextLine();
+	}
+	
+	// Constructor method that sets the hostname and portNumber variables.
+	public Client()
+	{
+		hostname = "localhost";
+		portNumber = 2066;
+	}
+	
+	// Core function that launches the client in the console window and handles it going forward.
+	public void launchClient()
+	{
 		try
 		{
+			// Connect to the server.
 			System.out.println("Connecting to chat server at " + hostname + " on port number " + portNumber);
 			socket = new Socket(hostname, portNumber);
 			System.out.println("Connected to server at " + socket.getRemoteSocketAddress());
 			
-			BufferedWriter output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			// Instantiate the PrintWriter and BufferedReader objects that will act as the input and output collectors.
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
 			BufferedReader serverInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			
-			while (socket.isConnected())
+			// Create a scanner object to allow the user to use the console.
+			Scanner input = new Scanner(System.in);
+			
+			// Set the user name and pass it to the server to be remembered.
+			String username = setUsername(input);
+			output.println(username);
+			
+			// Create a new ClientListener thread to output any messages.
+			Thread thread = new Thread(new ClientListener(serverInput));
+			thread.start();
+			
+			// Infinite loop that waits for the user to input a message, and then sends it to the server to be handled.
+			while (true)
 			{
-				Scanner input = new Scanner(System.in);
 				String message = input.nextLine();
-				
-				output.write(message);
-				output.flush();
+				output.println(message);
 			}
 		}
 		catch (IOException ex)
@@ -44,5 +71,12 @@ public class Client
 				System.err.println(ex.getCause() + ": " + ex.getMessage());
 			}
 		}
+	}
+	
+	// Main method that instantiates the Client class and then calls the core launchClient() function.
+	public static void main(String[] args) 
+	{
+		Client client = new Client();
+		client.launchClient();
 	}
 }
