d5e519fbbeb5b811c689b0c0d14f49e5bb02cf57
DanBest21 <44063921+DanBest21@users.noreply.github.com>
2018-11-02 00:12:46 +0000

    Updated Server class to handle multiple clients

--- Server.java
+++ Server.java
@@ -1,18 +1,23 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
-public class Server extends Thread
+// Class that handles the server-side of the chat application.
+public class Server
 {
+	// Instance variables
 	private static Integer portNumber = 2066;
-	private static Integer timeout = 20000;
 	private static ServerSocket serverSocket;
 	
+	private static HashMap<Socket, String> userList = new HashMap<Socket, String>();
+	private static HashSet<PrintWriter> printWriters = new HashSet<PrintWriter>();
+	
+	// Constructor that instantiates the ServerSocket object that Socket objects will be created from.
 	public Server()
 	{
 		try
 		{
 			serverSocket = new ServerSocket(portNumber);
-			serverSocket.setSoTimeout(timeout);
 		}
 		catch (IOException ex)
 		{
@@ -20,47 +25,54 @@ public class Server extends Thread
 		}
 	}
 	
-	public void run()
+	// Getter method that gets the user name associated to the Socket object passed.
+	public String getUsername(Socket socket)
 	{
-		Socket socket = null;
-			
-		try
+		return userList.get(socket);
+	}
+	
+	// Function that updates the userList HashMap, in order to associate a user name to each new client that connects to the server.
+	public void updateUserList(Socket socket, String username)
+	{
+		userList.put(socket, username);
+	}
+	
+	// Function that adds a PrintWriter attached to a client to the printWriters HashSet.
+	public void addWriter(PrintWriter writer)
+	{
+		printWriters.add(writer);
+	}
+	
+	// Method that sends each received message to every client except the one that sent/caused it.
+	public void sendMessage(String message, PrintWriter clientToIgnore)
+	{
+		// Loop through each print writer.
+		for (PrintWriter output : printWriters)
 		{
-			System.out.println("Server started on port number " + serverSocket.getLocalPort());
-				
-			while (true)
+			// Send the message unless this is the client that sent it.
+			if (output != clientToIgnore)
 			{
-				socket = serverSocket.accept();
-				
-				PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-				BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			
-				System.out.println(input.readLine());
-			}
-		}
-		catch (IOException ex)
-		{
-			System.err.println(ex.getCause() + ": " + ex.getMessage());
-		}
-		finally 
-		{
-			try
-			{
-				socket.close();
-			}
-			catch (IOException ex)
-			{
-				System.err.println(ex.getCause() + ": " + ex.getMessage());
+				output.println(message);
 			}
 		}
 	}
 	
+	// Main method that instantiates the server and creates new ServerThread objects every time a new client connects.
 	public static void main(String [] args)
 	{
 		try
-		{
-			Thread thread = new Server();
-			thread.start();
+		{			
+			Server server = new Server();
+			System.out.println("Server started on port number " + serverSocket.getLocalPort());
+			
+			// An infinite loop that will handle any new client that attempts to connect to the server.
+			while (true)
+			{
+				// Accept the connection, create a socket, and create a new ServerThread object to specifically handle that user.
+				Socket socket = serverSocket.accept();
+				Thread thread = new Thread(new ServerThread(server, socket));
+				thread.start();
+			}
 		}
 		catch (Exception ex)
 		{
